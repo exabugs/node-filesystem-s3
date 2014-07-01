f2067bc9666dcdc0845f609690c3ba3176cf2fe4
exabugs <exabugs@gmail.com>
2014-07-01 17:55:12 +0900

    Success. yahoo!!!

--- test/mongo-S3/test.js
+++ test/mongo-S3/test.js
@@ -7,7 +7,8 @@
 
 var test_backet = 'test_backet';
 
-var Base = require('../../lib/filesystem/s3-mongo');;
+var Base = require('../../lib/filesystem/s3-mongo');
+;
 
 var DB = require('../../lib/db');
 var fs = require('fs');
@@ -24,13 +25,11 @@ describe('Mongo', function () {
     });
   });
 
-  var fields = {
-    _id: 1,
-    filename: 1,
-    contentType: 1
-  };
-
-  it('Insert.', function (done) {
+  /**
+   * 追加
+   *   追加したい場合は、readstreamをbodyにセットする。
+   */
+  it('Insert & FindOne.', function (done) {
 
     var base = new Base(test_backet);
 
@@ -38,15 +37,22 @@ describe('Mongo', function () {
     var filepath = path.resolve(path.join('test', 'mongo-s3', filename));
     var stream = fs.createReadStream(filepath);
 
+    // 追加したい場合は、readstreamをbodyにセットする。
     var params = {
       filename: filename,
       body: stream,
       contentType: 'image/png'
     };
 
+    var get_fields = {
+      _id: 1,
+      filename: 1,
+      contentType: 1
+    };
+
     base.update(params, function (err, result) {
       var _id = result._id;
-      base.findOne({query: {_id: _id}, fields: fields}, function (err, result) {
+      base.findOne({query: {_id: _id}, fields: get_fields}, function (err, result) {
 
         var expect = {
           _id: result._id,
@@ -59,18 +65,48 @@ describe('Mongo', function () {
       });
     });
   });
-/*
-  it('List.', function (done) {
+
+  /**
+   * 取得
+   *   取得したい場合は、fieldsでbodyを1にする。
+   *    → 結果オブジェクトのbodyにreadstreamがセットされる。
+   */
+  it('Insert & FindOne (File Load Check).', function (done) {
 
     var base = new Base(test_backet);
 
-    base.update(params1, function (err, result) {
-      base.find({query: {contentType: 'text/html'}, fields: fields}, function (err, result) {
-        result.length.should.eql(1);
-        result[0].should.eql(params1);
-        done();
+    var filename = 'popy150.png';
+    var filepath0 = path.resolve(path.join('test', 'mongo-s3', filename));
+    var stream = fs.createReadStream(filepath0);
+
+    var params = {
+      filename: filename,
+      body: stream,
+      contentType: 'image/png'
+    };
+
+    var get_fields = {
+      _id: 1,
+      body: 1
+    };
+
+    base.update(params, function (err, result) {
+      var _id = result._id;
+      base.findOne({query: {_id: _id}, fields: get_fields}, function (err, result) {
+
+        // ストリーム処理が面倒なので一度ファイルに受ける
+        var filepath1 = path.resolve(path.join('logs', filename));
+        var ws = fs.createWriteStream(filepath1);
+        result.body.pipe(ws);
+
+        ws.on('close', function () {
+          var a0 = fs.readFileSync(filepath0).toString('base64');
+          var a1 = fs.readFileSync(filepath1).toString('base64');
+          should.equal(a0, a1);
+          done();
+        });
       });
     });
   });
-*/
+
 });
