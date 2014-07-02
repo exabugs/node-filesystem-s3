38441d6dfd72e38f54a6d4664b3c1fdf6bba96b5
exabugs <exabugs@gmail.com>
2014-07-02 17:08:59 +0900

    refactor.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 "use strict";
 
 exports.base = require('./lib/base');
-exports.DB = require('./lib/db');
+exports.Util = require('./lib/db');
 exports.filesystem = require('./lib/filesystem/s3-mongo');
 exports.S3 = require('./lib/filesystem/s3-native');
