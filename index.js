757146a6e2c3561d4403f4eb941840d4d21d7474
exabugs <exabugs@gmail.com>
2014-07-08 10:01:52 +0900

    取りやめ。

--- index.js
+++ index.js
@@ -4,4 +4,3 @@ exports.base = require('./lib/base');
 exports.Util = require('./lib/db');
 exports.filesystem = require('./lib/filesystem/s3-mongo');
 exports.S3 = require('./lib/filesystem/s3-native');
-exports.ObjectID = require('mongodb').ObjectID;
